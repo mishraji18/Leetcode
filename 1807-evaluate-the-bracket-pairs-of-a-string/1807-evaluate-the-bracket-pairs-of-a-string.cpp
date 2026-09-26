4dc75276e15fcb77148325a74abecbc17ec3d455
Devansh Mishra <240253@kit.ac.in>
2026-09-26 21:53:05 +0530

    Time: 77 ms (64.8%), Space: 122.4 MB (82.24%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+
+        unordered_map<string, string> mp;
+
+        for (const auto& it : knowledge) {
+            mp.emplace(it[0], it[1]);
+        }
+
+        string ans;
+        ans.reserve(s.size());
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (s[i] != '(') {
+                ans += s[i];
+                continue;
+            }
+
+            string key;
+
+            while (s[++i] != ')') {
+                key += s[i];
+            }
+
+            auto it = mp.find(key);
+
+            if (it != mp.end())
+                ans += it->second;
+            else
+                ans += '?';
+        }
+
+        return ans;
+    }
+};
